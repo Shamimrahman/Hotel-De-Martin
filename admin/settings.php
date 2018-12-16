17a42dddb0f3c96ec1768e7f1adba500f0edc460
Talbia Kabir <talbia165@gmail.com>
2018-12-16 17:00:34 +0600

    Update settings.php
    Just Superior and Delux room code added

--- admin/settings.php
+++ admin/settings.php
@@ -72,3 +72,62 @@ if(!isset($_SESSION["user"]))
 
                     
             </div>
+		        <?php
+						include ('db.php');
+						$sql = "select * from room";
+						$re = mysqli_query($con,$sql)
+				?>
+                <div class="row">
+				
+				
+				<?php
+										while($row= mysqli_fetch_array($re))
+										{
+												$id = $row['type'];
+											if($id == "Superior Room") 
+											{
+												echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+													<div class='panel panel-primary text-center no-boder bg-color-blue'>
+														<div class='panel-body'>
+															<i class='fa fa-users fa-5x'></i>
+															<h3>".$row['bedding']."</h3>
+														</div>
+														<div class='panel-footer back-footer-blue'>
+															".$row['type']."
+
+														</div>
+													</div>
+												</div>";
+											}
+											else if ($id == "Deluxe Room")
+											{
+												echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+													<div class='panel panel-primary text-center no-boder bg-color-green'>
+														<div class='panel-body'>
+															<i class='fa fa-users fa-5x'></i>
+															<h3>".$row['bedding']."</h3>
+														</div>
+														<div class='panel-footer back-footer-green'>
+															".$row['type']."
+
+														</div>
+													</div>
+												</div>";
+											
+											}
+											else if($id =="Guest House")
+											{
+												echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+													<div class='panel panel-primary text-center no-boder bg-color-brown'>
+														<div class='panel-body'>
+															<i class='fa fa-users fa-5x'></i>
+															<h3>".$row['bedding']."</h3>
+														</div>
+														<div class='panel-footer back-footer-brown'>
+															".$row['type']."
+
+														</div>
+													</div>
+												</div>";
+											
+											}
