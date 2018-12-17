533bd36cd2135e0fd3eb6bffe27443377fe9ac39
Ekram Ul Ratul <45538412+Ekramm@users.noreply.github.com>
2018-12-18 01:03:42 +0600

    Update settings.php

--- admin/settings.php
+++ admin/settings.php
@@ -131,3 +131,25 @@ if(!isset($_SESSION["user"]))
 												</div>";
 											
 											}
+											else if($id =="Guest House")
+											{
+												echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+													<div class='panel panel-primary text-center no-boder bg-color-brown'>
+														<div class='panel-body'>
+															<i class='fa fa-users fa-5x'></i>
+															<h3>".$row['bedding']."</h3>
+														</div>
+														<div class='panel-footer back-footer-brown'>
+															".$row['type']."
+
+														</div>
+													</div>
+												</div>";
+											
+											}
+											
+											
+											}
+
+
+
