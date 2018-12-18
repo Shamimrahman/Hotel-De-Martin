cec7e11ce85b219b13d91c56e2043fce4fded9f0
MD.Shamim Rahman <shamimrocky801@yahoo.com>
2018-12-19 02:19:25 +0600

    Change admin and password

---
+++ admin/usersetting.php
@@ -0,0 +1,159 @@
+<?php  
+session_start();  
+if(!isset($_SESSION["user"]))
+{
+ header("location:index.php");
+}
+
+ob_start();
+?> 
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Hotel De Martin</title>
+	<!-- Bootstrap Styles-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FontAwesome Styles-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- Custom Styles-->
+    <link href="assets/css/custom-styles.css" rel="stylesheet" />
+     <!-- Google Fonts-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    <link href="assets/js/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default top-navbar" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="home.php">MAIN MENU </a>
+            </div>
+
+            <ul class="nav navbar-top-links navbar-right">
+			
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="usersetting.php"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="settings.php"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+					
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+        </nav>
+        <!--/. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+
+                    <li>
+                        <a class="active-menu" href="settings.php"><i class="fa fa-dashboard"></i>User Dashboard</a>
+                    </li>
+					
+					
+
+                    
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+       
+        <div id="page-wrapper" >
+            <div id="page-inner">
+			 <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-header">
+                           ADMINISTRATOR<small> accounts </small>
+                        </h1>
+                    </div>
+                </div> 
+                 
+                                 
+            <?php
+						include ('db.php');
+						$sql = "SELECT * FROM `login`";
+						$re = mysqli_query($con,$sql)
+				?>
+                
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- Advanced Tables -->
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>User ID</th>
+											<th>User name</th>
+                                            <th>Password</th>
+                                            
+											<th>Update</th>
+											<th>Remove</th>
+                                            
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        
+									<?php
+										while($row = mysqli_fetch_array($re))
+										{
+										
+											$id = $row['id'];
+											$us = $row['usname'];
+											$ps = $row['pass'];
+											if($id % 2 ==0 )
+											{
+												echo"<tr class='gradeC'>
+													<td>".$id."</td>
+													<td>".$us."</td>
+													<td>".$ps."</td>
+													
+													<td><button class='btn btn-primary btn' data-toggle='modal' data-target='#myModal'>
+															 Update 
+													</button></td>
+													<td><a href=usersettingdel.php?eid=".$id ." <button class='btn btn-danger'> <i class='fa fa-edit' ></i> Delete</button></td>
+												</tr>";
+											}
+											else
+											{
+												echo"<tr class='gradeU'>
+													<td>".$id."</td>
+													<td>".$us."</td>
+													<td>".$ps."</td>
+													
+													<td><button class='btn btn-primary btn' data-toggle='modal' data-target='#myModal'>
+                              Update 
+                            </button></td>
+													<td><a href=usersettingdel.php?eid=".$id ." <button class='btn btn-danger'> <i class='fa fa-edit' ></i> Delete</button></td>
+												</tr>";
+											
+											}
+										
+										}
+										
+									?>
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
