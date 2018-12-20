0627825e301733dd4c7631c05082f65161910d51
MD.Shamim Rahman <shamimrocky801@yahoo.com>
2018-12-21 02:53:17 +0600

    Booking info added

---
+++ admin/roombook.php
@@ -0,0 +1,73 @@
+<?php  
+session_start();  
+if(!isset($_SESSION["user"]))
+{
+ header("location:index.php");
+}
+?> 
+
+<?php
+		if(!isset($_GET["rid"]))
+		{
+				
+			 header("location:index.php");
+		}
+		else {
+				$curdate=date("Y/m/d");
+				include ('db.php');
+				$id = $_GET['rid'];
+				
+				
+				$sql ="Select * from roombook where id = '$id'";
+				$re = mysqli_query($con,$sql);
+				while($row=mysqli_fetch_array($re))
+				{
+					$title = $row['Title'];
+					$fname = $row['FName'];
+					$lname = $row['LName'];
+					$email = $row['Email'];
+					$nat = $row['National'];
+					$country = $row['Country'];
+					$Phone = $row['Phone'];
+					$troom = $row['TRoom'];
+					$nroom = $row['NRoom'];
+					$bed = $row['Bed'];
+					$non = $row['NRoom'];
+					$meal = $row['Meal'];
+					$cin = $row['cin'];
+					$cout = $row['cout'];
+					$sta = $row['stat'];
+					$days = $row['nodays'];
+					
+				
+				
+				}
+					
+					
+				
+		
+	}
+		
+		
+		
+			?> 
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Administrator	</title>
+    <!-- Bootstrap Styles-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FontAwesome Styles-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- Morris Chart Styles-->
+    <link href="assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <!-- Custom Styles-->
+    <link href="assets/css/custom-styles.css" rel="stylesheet" />
+    <!-- Google Fonts-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+
