ad3d0b3f3d66a3b09c575fef0c4a4621591f8a50
MD.Shamim Rahman <shamimrocky801@yahoo.com>
2018-12-21 18:42:45 +0600

    room.php

---
+++ admin/room.php
@@ -0,0 +1,112 @@
+<?php  
+session_start();  
+if(!isset($_SESSION["user"]))
+{
+ header("location:index.php");
+}
+?> 
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SUNRISE HOTEL</title>
+	<!-- Bootstrap Styles-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FontAwesome Styles-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- Custom Styles-->
+    <link href="assets/css/custom-styles.css" rel="stylesheet" />
+     <!-- Google Fonts-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default top-navbar" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="home.php">MAIN MENU </a>
+            </div>
+
+            <ul class="nav navbar-top-links navbar-right">
+			
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="usersetting.php"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="settings.php"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+					
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+        </nav>
+        <!--/. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+
+                    <li>
+                        <a  href="settings.php"><i class="fa fa-dashboard"></i>Rooms Status</a>
+                    </li>
+					<li>
+                        <a  class="active-menu" href="room.php"><i class="fa fa-plus-circle"></i>Add Room</a>
+                    </li>
+                    <li>
+                        <a  href="roomdel.php"><i class="fa fa-desktop"></i> Delete Room</a>
+                    </li>
+					
+
+                    
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+       
+        
+       
+        <div id="page-wrapper" >
+            <div id="page-inner">
+			 <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-header">
+                           NEW ROOM <small></small>
+                        </h1>
+                    </div>
+                </div> 
+                 
+                                 
+            <div class="row">
+                
+                <div class="col-md-5 col-sm-5">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            ADD NEW ROOM
+                        </div>
+                        <div class="panel-body">
+						<form name="form" method="post">
+                            <div class="form-group">
+                                            <label>Type Of Room *</label>
+                                            <select name="troom"  class="form-control" required>
+												<option value selected ></option>
+                                                <option value="Superior Room">SUPERIOR ROOM</option>
+                                                <option value="Deluxe Room">DELUXE ROOM</option>
+												<option value="Guest House">GUEST HOUSE</option>
+												<option value="Single Room">SINGLE ROOM</option>
+                                            </select>
+                              </div>
+							  
+					
